da3fb72b99dd1a31e1a5b06a21eb2f1a23bd045d
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2023-07-03 16:55:41 -0300

    Atividade de um apricativo, rock in rio

---
+++ app.flutter/lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RockInRio());
+}
+
+class RockInRio extends StatelessWidget {
+  const RockInRio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rock in Rio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Atracao> _listaFavoritos = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Atrações'),
+      ),
+      body: ListView.builder(
+        itemCount: listaAtracoes.length,
+        itemBuilder: (context, index) {
+          final isFavorito = _listaFavoritos.contains(listaAtracoes[index]);
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          AtracaoPage(atracao: listaAtracoes[index])));
+            },
+            title: Text(listaAtracoes[index].nome),
+            subtitle: Wrap(
+              spacing: 8,
+              runSpacing: 4,
+              children: listaAtracoes[index]
+                  .tags
+                  .map((tag) => Chip(label: Text('#$tag')))
+                  .toList(),
+            ),
+            leading: CircleAvatar(
+              child: Text('${listaAtracoes[index].dia}'),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  if (isFavorito) {
+                    _listaFavoritos.remove(listaAtracoes[index]);
+                  } else {
+                    _listaFavoritos.add(listaAtracoes[index]);
+                  }
+                });
+              },
+              icon: isFavorito
+                  ? const Icon(Icons.favorite, color: Colors.red)
+                  : const Icon(Icons.favorite_border),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class AtracaoPage extends StatelessWidget {
+  final Atracao atracao;
+  const AtracaoPage({Key? key, required this.atracao}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(atracao.nome),
+      ),
+      body: ListView.builder(
+  
+  itemBuilder: (context, index) {
+    return ListTile(
+      title: Text(atracao.nome),
+    );
+  },
+),
+    );
+  }
+}
+
+class Atracao {
+  final String nome;
+  final int dia;
+  final List<String> tags;
+
+  const Atracao(this.nome, this.dia, this.tags);
+}
+
+const listaAtracoes = [
+  Atracao("Iron Maiden", 2, ["Espetaculo", "Fas", "NovoAlbum"]),
+  Atracao("Alok", 3, ["Influente", "Top", "Show"]),
+  Atracao("Justin Bieber", 4, ["TopCharts", "Hits", "PríncipeDoPOP"]),
+  Atracao("Guns N Roses", 8, ["Sucesso", "Espetáculo", "Fas"]),
+  Atracao("Capital Inicial", 9, ["2019", "Novo Álbum", "Fas"]),
+  Atracao("Green Day", 9, ["Sucesso", "Reconhecimento", "Show"]),
+  Atracao("Cold Play", 10, ["NovoAlbum", "Sucesso", "2011"]),
+  Atracao("Ivete Sangalo", 10, ["Unica", "Carreiras", "Fas"]),
+  Atracao("Racionais", 3, ["Hits", "Prêmios", "Respeito"]),
+  Atracao("Gloria Groove", 8, ["Streams", "Representatividade", "Sucesso"]),
+  Atracao("Avril Lavigne", 9, ["Estreia", "Sucesso", "Lançamento"]),
+  Atracao("Ludmilla", 10, ["Representativade", "Sucesso", "Parcerias"]),
+];
